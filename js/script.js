b0626477acc63616a2fb5099a84d5036af74aa8d
Allan <teixeiraallan312@gmail.com>
2025-05-27 22:15:29 -0300

    implementando o mysql

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("Deu certo")
+
+var formulario = document.getElementById("form-cadastro")
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    var campoNome = document.getElementById("nome").value
+    var campoEmail = document.getElementById("email").value
+
+    console.log(campoNome,campoEmail)
+})
